8c656d5d960f8314ebef9a8c5ded0524ebd7c098
Alewesley-Sousa <alewesley1234@gmail.com>
2026-04-27 13:31:27 -0300

    [FEAT] instalado o swagger para testar as rotas apis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevelopersModule } from './developers/developers.module';
+import { ProjetosModule } from './projetos/projetos.module';
 
 @Module({
-  imports: [DevelopersModule],
+  imports: [DevelopersModule, ProjetosModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Permite que o frontend acesse a API
   app.enableCors();
+
+  // --- CONFIGURAÇÃO DO SWAGGER ---
+  const config = new DocumentBuilder()
+    .setTitle('API SECTEC')
+    .setDescription('Sistema de Gerenciamento de Projetos - SECTEC')
+    .setVersion('1.0')
+    .addTag('projetos') // Organiza as rotas por tag
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+  // O link será: http://localhost:3000/api
+  // -------------------------------
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Servidor rodando em: http://localhost:3000/api`);
 }
 bootstrap();
