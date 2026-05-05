81dc01ecb6f5d927468fa179c557a54936a2ae94
morkis-s <goldennascimento@gmail.com>
2026-05-05 20:57:52 -0300

    Adicionando o Aluno e A comunicação do back ja funcionando

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/login';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/DashboardAluno';
 import DashboardProfessor from './pages/DashboardProfessor';
 
 function App() {
