2e794e17381b068c32f4bc01f49f3c446f153197
Alewesley-Sousa <alewesley1234@gmail.com>
2026-07-08 10:49:42 -0300

    FIX: correção de importação bcryptjs

--- backend/src/users/users.seed.ts
+++ backend/src/users/users.seed.ts
@@ -1,23 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User, UserRole } from './entities/user.entity'; 
-import * as bcrypt from 'bcrypt';
+import { User, UserRole } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersSeed {
   constructor(
     @InjectRepository(User)
     private readonly usuarioRepository: Repository<User>,
-  ) {}
-
-  async run() {
-    const salt = await bcrypt.genSalt(10);
-    const senhaHashed = await bcrypt.hash('Senha123@', salt);
-    
-    console.log('---------------------------------------');
-    console.log('✅ Seeds finalizadas com sucesso!');
-    console.log('🔑 Senha padrão: Senha123@');
-    console.log('---------------------------------------');
-  }
+  ) { }
 }
