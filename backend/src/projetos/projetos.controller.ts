4c6f3bdeb6859b0cdc5f396b83a8246c250b000f
Alewesley-Sousa <alewesley1234@gmail.com>
2026-08-19 19:37:52 -0300

    corrijido

--- backend/src/projetos/projetos.controller.ts
+++ backend/src/projetos/projetos.controller.ts
@@ -109,6 +109,8 @@ export class ProjetosController {
     res.set({
       'Content-Type': 'application/pdf',
       'Content-Disposition': `inline; filename="${encodeURIComponent(originalName)}"`,
+      'X-Frame-Options': 'ALLOWALL',
+      'Content-Security-Policy': "frame-ancestors 'self' https://sectec-publico.up.railway.app",
     });
 
     stream.pipe(res);
